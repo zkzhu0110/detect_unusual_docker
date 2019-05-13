8ea5965254e7c194531f3085d95abffeb776e106
root <2469209157@qq.com>
2019-05-13 20:34:56 +0800

    delete wrong dir

--- modify_prometheus_yml/app/login/config.py
+++
@@ -1,9 +0,0 @@
-#encoding=utf-8
-SECRET_KEY = 'jklklsadhfjkhwbii9/sdf\sdf'
-db_config = {
-        'db_host': '10.12.3.5',
-        'db_port': 3306,
-        'db_user': 'root',
-        'db_pwd': '12345678',
-        'database': 'management'
-    }
